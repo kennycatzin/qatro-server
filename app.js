5bca2aecd439ca7e0d7d0a9603419c06250d448f
Kenny Catzin Ruiz <kenn2506@gmail.com>
2019-10-10 14:05:27 -0500

    hola

--- app.js
+++ app.js
@@ -3,10 +3,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 //Iniciar variables
 
 var app = express();
+
 
 
 // CORS
@@ -32,12 +34,22 @@ var imagenesRoutes = require('./routes/imagenes');
 // mongodb+srv://serteza:aleatorio2506@cluster0-vdo2o.mongodb.net/test?retryWrites=true&w=majority
 // entorno local: mongodb://localhost:27017/backend
 // mongodb + srv: //serteza:<password>@cluster0-vdo2o.mongodb.net/test?retryWrites=true&w=majority
-//Conexion a la base de datos
-mongoose.connection.openUri('mongodb+srv://serteza:aleatorio2506@cluster0-vdo2o.mongodb.net/qatro-db?retryWrites=true&w=majority', (err, res) => {
-    if (err) throw err;
-    console.log('Base de datos:  \x1b[32m%s\x1b[0m', 'online');
+// Conexion a la base de datos
 
+const uri = "mongodb+srv://serteza:aleatorio2506@cluster0-vdo2o.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
 });
+
+// mongoose.connection.openUri('mongodb://localhost:27017/qatro-backend', (err, res) => {
+//     if (err) throw err;
+//     console.log('Base de datos:  \x1b[32m%s\x1b[0m', 'online');
+
+// });
+
 // Server index config
 var serveIndex = require('serve-index');
 app.use(express.static(__dirname + '/'));
